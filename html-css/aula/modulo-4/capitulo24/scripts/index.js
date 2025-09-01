9024086f2dde0fc5a5e25fe6f52c709759701471
TalDoJulio <157721108+TalDoJulio@users.noreply.github.com>
2025-09-01 19:22:59 -0300

    save

---
+++ html-css/aula/modulo-4/capitulo24/scripts/index.js
@@ -0,0 +1,6 @@
+function blurInput(input) {
+    const idLabel = 'label-' + input.name
+    const isValue = input.value != ''
+    const label = document.getElementById(idLabel)
+    label.classList = isValue ? 'active' : ''
+}
